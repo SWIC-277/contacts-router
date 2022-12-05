2bd2571e6f83f11a6a28ba780ae5be6e80a69d7b
Manav Misra <manavm@visionwebsoft.com>
2022-12-04 18:47:53 -0600

    refactor: :coffin: conditional ⛓️ chaining
    Not needed with 'useLoaderData'!

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
   return (
     <nav className="grow border-y-2 border-gray-300 py-2">
       <ul>
-        {contacts?.map((contact) => {
+        {contacts.map((contact) => {
           return (
             <li key={contact.id} className="pointer my-4 hover:text-blue-500">
               <Link to={`/contacts/${contact.id}`}>
