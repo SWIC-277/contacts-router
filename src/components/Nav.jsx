4c78b97b1210c20b33e909fe7b766dcacfc12761
Manav Misra <manavm@visionwebsoft.com>
2022-12-04 18:50:42 -0600

    feat: :sparkles: filter nav list based on query

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,19 @@
 import { Link, useLoaderData } from "react-router-dom";
 
 export default function Nav() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
+
+  const filteredContacts = q
+    ? contacts.filter(
+        (contact) =>
+          contact.firstName.includes(q) || contact.lastName.includes(q)
+      )
+    : contacts;
 
   return (
     <nav className="grow border-y-2 border-gray-300 py-2">
       <ul>
-        {contacts.map((contact) => {
+        {filteredContacts.map((contact) => {
           return (
             <li key={contact.id} className="pointer my-4 hover:text-blue-500">
               <Link to={`/contacts/${contact.id}`}>
